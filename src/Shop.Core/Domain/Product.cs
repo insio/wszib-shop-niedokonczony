3bc5486e98ec2b653c213cb13af63d45e1a09685
k75072403073 <k75072403073@KOMP409-13>
2018-01-13 12:17:36 +0100

    nie wiem WTF?!

---
+++ src/Shop.Core/Domain/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Core.Domain
+{
+    public class Product
+    {
+
+        public string Name { get; }
+        public Guid Id { get; }
+        public string Category { get; }
+        public decimal Price { get; }
+
+        public Product(string name, string category, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Category = category;
+            Price = price;
+
+        }
+    }
+}
