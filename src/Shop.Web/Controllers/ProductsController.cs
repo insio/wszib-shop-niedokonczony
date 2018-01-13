7953eaa981932d8087665fabda3bb718c35b7231
k75072403073 <k75072403073@KOMP409-13>
2018-01-13 12:35:24 +0100

    cd

---
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+
+namespace Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+
+    {
+        private static readonly List<Product> _products = new List<Product>
+        {
+            new Product ("Laptop", "Electronics", 3000),
+            new Product ("Spodnie", "Trousers", 3000),
+            new Product ("Hammer", "Tools", 3000),
+
+        };
+
+    
+
+    
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
