c55af590414d04c86a23ff0e095c5747cccaf9e2
k75072403073 <k75072403073@KOMP409-13>
2018-01-13 16:47:59 +0100

    niedokonczone

---
+++ src/Shop.Core/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.Core.Domain;
+using System.Linq;
+
+namespace Shop.Core.Repositories
+{
+    class ProductRepository : IProductRepository
+    {
+        private readonly ISet<Product> _products = new HashSet<Product>();
+
+        public Product Get(Guid Id)
+       
+           => _products.SingleOrDefault(x => x.Id == Id);
+
+
+        public IEnumerable<Product> GetAll()
+        => _products;
+
+        public void Add(Product product)
+
+            => _products.Add(product);
+    }
+}
